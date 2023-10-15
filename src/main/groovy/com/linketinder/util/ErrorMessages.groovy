20dedf3f7862b93bc3cbdd02e6f4ee61d9d46f80
Will Oliveira <will.oliveira1@hotmail.com>
2023-10-14 23:37:23 -0300

    [REFACTOR] Refatorado as classes utilizando Clean Code e SOLID.

--- src/main/groovy/com/linketinder/util/ErrorMessages.groovy
+++ src/main/groovy/com/linketinder/util/ErrorMessages.groovy
@@ -2,6 +2,6 @@ package com.linketinder.util
 
 class ErrorMessages {
 
-    public static final String DBMSG = "Não foi possível concluir a operação no banco de dados."
+    public static final String DB_MSG = "Não foi possível concluir a operação no banco de dados."
 
 }
