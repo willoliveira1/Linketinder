d8e672c0eb4cccb763633c97f5e37a38c8da37d5
Will Oliveira <will.oliveira1@hotmail.com>
2023-10-21 09:25:45 -0300

    [FEATURE] Implementado o design-pattern Factory Method na conexão com o banco de dados.

---
+++ src/main/groovy/com/linketinder/database/ConnectionFactory.groovy
@@ -0,0 +1,19 @@
+package com.linketinder.database
+
+import com.linketinder.database.interfaces.IConnection
+
+abstract class ConnectionFactory {
+
+    private ConnectionFactory() {
+    }
+
+    static IConnection createConnection(String dbType) {
+        switch (dbType) {
+            case "POSTGRESQL":
+                return new PostgreSqlConnection()
+            default:
+                throw new RuntimeException("Invalid database")
+        }
+    }
+
+}
