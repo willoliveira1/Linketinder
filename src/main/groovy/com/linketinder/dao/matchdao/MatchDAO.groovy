3908b1178117d5b8ea4ae7326568708037a33b73
Will Oliveira <will.oliveira1@hotmail.com>
2023-11-01 09:09:38 -0300

    [REFACTOR] Refatorado as DAOs isolando as queries.

--- src/main/groovy/com/linketinder/dao/matchdao/MatchDAO.groovy
+++ src/main/groovy/com/linketinder/dao/matchdao/MatchDAO.groovy
@@ -14,10 +14,6 @@ import java.util.logging.Logger
 
 class MatchDAO implements IMatchDAO {
 
-    private final String GET_ALL_MATCHES = "SELECT id, candidate_id, company_id, job_vacancy_id FROM matches WHERE company_id IS NOT NULL AND job_vacancy_id IS NOT NULL ORDER BY id"
-    private final String UPDATE_MATCH = "UPDATE matches SET candidate_id=?, company_id=?, job_vacancy_id=? WHERE id=?"
-
-
     IConnection connection
     Sql sql = connection.instance()
 
