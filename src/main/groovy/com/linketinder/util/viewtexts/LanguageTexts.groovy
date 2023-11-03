4f0a5aef2ee4b28881171b3597cfbecac241d9d2
Will Oliveira <will.oliveira1@hotmail.com>
2023-11-03 07:21:48 -0300

    [FEATURE] Implementado REST API.

--- src/main/groovy/com/linketinder/util/viewtexts/LanguageTexts.groovy
+++
@@ -1,9 +0,0 @@
-package com.linketinder.util.viewtexts
-
-class LanguageTexts {
-
-    public static final String ADD_LANGUAGE_TEXT = "Deseja adicionar idiomas? (S/N)"
-    public static final String NAME_TEXT = "Qual o idioma?"
-    public static final String ADD_MORE_LANGUGAGE_TEXT = "Deseja adicionar mais idiomas? (S/N)"
-
-}
